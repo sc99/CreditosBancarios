2cca164ff6762a9145d1e548b548c4d7282b1ab9
Kiara <kiara.itan18@gmail.com>
2018-11-13 01:57:26 -0600

    Inicio de las vistas

---
+++ com/creditosBancarios/api/views/telephone.research.views.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-with, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../../com/creditosBancarios/util/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../../com/creditosBancarios/util/assets/css/style.css">
+    <script src="../../../../com/creditosBancarios/util/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../../../../com/creditosBancarios/util/assets/js/bootstrap.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+
+      <header class="row">
+        <div id="logo" >
+        </div>
+
+        <div id="login"class="col-lg-9 col-md-9 sm-8 col-xs-12 row">
+          <aside class="">
+            <p id="nameManager">Nombre de empleado</p>
+            <button id="logOff"class="btn btn-primary" type="button" name="button">Cerrar sesión</button>
+          </aside>
+
+          <aside class="">
+            <div class="pos-f-t">
+              <div class="collapse" id="navbarToggleExternalContent">
+                <div class="bg-dark p-4">
+                  <h4 class="text-white">Collapsed content</h4>
+                  <span class="text-muted">Toggleable via the navbar brand.</span>
+                </div>
+              </div>
+              <nav class="navbar navbar-dark bg-dark">
+                <button class="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarToggleExternalContent"   aria-controls="navbarToggleExternalContent" aria-expanded="false"   aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </nav>
+            </div>
+          </aside>
+
+        </div>
+      </header>
+
+      <!--ComboBox de empleado-->
+      <div id="cmbManager" class="col-lg-10 col-md-12 sm-12 col-xs-12 ">
+        <select class="form-control">
+          <option id="telephoneResearch" value="">Investigación Telefonica</option>
+          <option id="notificationCostumer" value="">Notificación para usuario</option>
+          <option id="observationRequest" value="">Observación de solicitudes pendientes</option>
+        </select>
+      </div>
+
+      <div class="">
+        <button id="accept"class="btn btn-primary" type="button" name="button">Aceptar</button>
+      </div>
+
+      <!--Barra de busqueda-->
+      <div id="searchIdCostumer"class="col-lg-10 col-md-12 sm-12 col-xs-12 ">
+        <nav class="">
+          <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search"   placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0"  type="submit">Search</button>
+          </form>
+        </nav>
+      </div>
+
+      <!--ComboBox de id de clientes-->
+      <div id="cmbIdCostumer" class="col-lg-10 col-md-12 sm-12 col-xs-12 ">
+        <select class="form-control">
+          <option id="Id1" value="">Nombre de cliente ID1</option>
+          <option id="Id2" value="">Nombre de cliente ID2</option>
+          <option id="Id3" value="">Nombre de cliente ID3 </option>
+        </select>
+      </div>
+
+      <!--Boton para seleccionar un cliente-->
+      <div class="">
+        <button id="selectIdCostumer"class="btn btn-primary" type="button" name="button">Seleccionar</button>
+      </div>
+
+
+
+    </div>
+  </body>
+</html>
---
+++ com/creditosBancarios/api/views/template.views.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-with, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../../com/creditosBancarios/util/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../../../com/creditosBancarios/util/assets/css/style.css">
+    <script src="../../../../com/creditosBancarios/util/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../../../../com/creditosBancarios/util/assets/js/bootstrap.min.js"></script>
+    <title></title>
+  </head>
+  <body>
+    <div class="container">
+
+      <header class="row">
+        <div id="logo" >
+        </div>
+
+        <div id="login"class="col-lg-9 col-md-9 sm-8 col-xs-12 row">
+          <aside class="">
+            <p id="costumer">Clientes</p>
+            <button id="loginCostumer"class="btn btn-primary" type="button" name="button">Iniciar sesion</button>
+          </aside>
+
+          <aside class="">
+            <p id="manager">Empleados</p>
+            <button id="loginManager"class="btn btn-primary" type="button" name="button">Iniciar sesion</button>
+          </aside>
+
+        </div>
+      </header>
+
+      <div id="center" class="col-lg-10 col-md-12 sm-12 col-xs-12 ">
+
+      </div>
+
+    </div>
+  </body>
+</html>
